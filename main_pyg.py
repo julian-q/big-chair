4ef7d56b537422263cfca74f5e6d01a2604100e3
Pino Cholsaipant <pinocholsaipant@DNa1d44ce.SUNet>
2022-02-24 01:05:02 -0800

    debugged tokenizer settings

--- main_pyg.py
+++ main_pyg.py
@@ -41,8 +41,7 @@ for epoch in range(EPOCH):
         #                         dim=0).to(device)
 
         batch_texts = torch.cat([model.tokenizer(model_descs, return_tensors="pt", padding=True)
-                                for model_descs in batch.descs],
-                                dim=0).to(device)
+                                for model_descs in batch.descs], dim=0).to(device)
 
         logits_per_mesh, logits_per_text = model(batch, batch_texts)
         # uniform distribution over matching descs
