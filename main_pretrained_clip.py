4f3d76315865654e35b364b3a843f9612664811c
Pino Cholsaipant <pinocholsaipant@DNa1de3aa.SUNet>
2022-02-25 01:38:52 -0800

    trying evaluate function

--- main_pretrained_clip.py
+++ main_pretrained_clip.py
@@ -16,15 +16,15 @@ EPOCH = 50
 dataset_root = './dataset/'
 # assumes that ./dataset/raw/ is full of .obj files!!!
 dataset = AnnotatedMeshDataset(dataset_root)
-train_dataset = dataset
-# dataset.shuffle()
-#
-# train_share = int(len(dataset) * 0.7)
-# val_share = int(((len(dataset) - train_share) * 2) / 3)
-#
-# train_dataset = dataset[: train_share]
-# val_dataset = dataset[train_share: train_share + val_share]
-# test_dataset = dataset[train_share + val_share: ]
+
+dataset.shuffle()
+
+train_share = 100#int(len(dataset) * 0.7)
+val_share = 10 #int(((len(dataset) - train_share) * 2) / 3)
+
+train_dataset = dataset[: train_share]
+val_dataset = dataset[train_share: train_share + val_share]
+test_dataset = dataset[train_share + val_share: ]
 
 train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=False)
 
@@ -43,6 +43,44 @@ def eval(logits_per_text, targets_per_text):
     preds = logits_per_text.argmax(dim=1)
     labels = targets_per_text.argmax(dim=1)
     return torch.sum(preds == labels) / labels.shape[0]
+
+def evaluate(eval_dataset, model, writer, epoch):
+    model.load_state_dict(torch.load("parameters.pt"))
+    model.eval()
+
+    eval_dataloader = DataLoader(eval_dataset, batch_size=len(eval_dataset), shuffle=False)
+
+    for i_batch, batch in enumerate(eval_dataloader):
+        n_batch = batch.batch.max() + 1
+        # now, batch contains a mega graph containing each
+        # graph in the batch, as usual with pyg data loaders.
+        # each of these graphs has a 'descs' array containing
+        # its descriptions, all of which get combined into one
+        # giant nested array. we tokenize them below:
+        batch.to(device)
+        # we could honestly move this code into the model's forward
+        # function now that we're using pyg
+        batch_texts = torch.cat([model.tokenizer(model_descs, return_tensors="pt", padding='max_length',
+                                                 truncation=True).input_ids
+                                 for model_descs in batch.descs], dim=0).to(device)
+        # vector mapping each description to its mesh index
+        desc2mesh = torch.zeros(batch_texts.shape[0], dtype=torch.long)
+        # uniform distribution over matching descs
+        target_per_mesh = torch.zeros(n_batch, batch_texts.shape[0]).to(device)
+        # one-hot distribution for single matching shape
+        target_per_text = torch.zeros(batch_texts.shape[0], n_batch).to(device)
+        # loop over the descriptions and populate above
+        i_desc = 0
+        for i_mesh, model_descs in enumerate(batch.descs):
+            desc2mesh[i_desc:i_desc + len(model_descs)] = i_mesh
+            target_per_text[i_desc:i_desc + len(model_descs), i_mesh] = 1
+            i_desc += len(model_descs)
+
+        logits_per_mesh, logits_per_text = model(batch, batch_texts, desc2mesh)
+        eval_acc = eval(logits_per_text, target_per_text)
+        print('eval accuracy:', eval_acc.item())
+        writer.add_scalar('Accu/val', eval.acc.item(), epoch)
+
 
 writer = SummaryWriter()
 total_loss = torch.tensor([0], dtype=torch.float).to(device)
@@ -81,7 +119,7 @@ for epoch in range(EPOCH):
         logits_per_mesh, logits_per_text = model(batch, batch_texts, desc2mesh)
         acc = eval(logits_per_text, target_per_text)
         writer.add_scalar('Accu/train', acc.item(), count)
-        print('train accuracy:', acc)
+        print('train accuracy:', acc.item())
         total_loss += (loss_mesh(logits_per_mesh, target_per_mesh) + loss_text(logits_per_text, target_per_text)) / 2
         if i_batch % 10 == 9:
             total_loss.backward()
@@ -91,11 +129,12 @@ for epoch in range(EPOCH):
             grad_step += 1
             total_loss = torch.tensor([0], dtype=torch.float).to(device)
             torch.save(model.state_dict(), "parameters.pt")
+            evaluate(val_dataset, model, writer, epoch)
         count += 1
 
 torch.save(model.state_dict(), "parameters.pt")
 
-evaluate(val_dataset, model, device="cpu")
+
 
 
 
