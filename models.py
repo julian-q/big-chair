7089d5231076be57589d8e8f5fd7fedf28d57bb7
sidsquid <sidney.hough@gmail.com>
2022-03-12 00:48:00 -0800

    add edge attr to mesh encoder

--- models.py
+++ models.py
@@ -141,19 +141,19 @@ class HierarchicalMeshEncoder(nn.Module):
 		x = F.elu(x)
 		x = F.dropout(x, p=self.dropout_prob, training=self.training)
 
-		x, edge_index, _, batch, _ = self.pool1(x, edge_index, None, batch)
+		x, edge_index, edge_attr, batch, _ = self.pool1(x, edge_index, edge_attr, batch)
 
-		x = self.conv2(x, edge_index, None)
+		x = self.conv2(x, edge_index, edge_attr)
 		x = F.elu(x)
 		x = F.dropout(x, p=self.dropout_prob, training=self.training)
 
-		x, edge_index, _, batch, _ = self.pool2(x, edge_index, None, batch)
+		x, edge_index, _, batch, _ = self.pool2(x, edge_index, edge_attr, batch)
 
-		x = self.conv3(x, edge_index, None)
+		x = self.conv3(x, edge_index, edge_attr)
 		x = F.elu(x)
 		x = F.dropout(x, p=self.dropout_prob, training=self.training)
 
-		x, edge_index, _, batch, _ = self.pool3(x, edge_index, None, batch)
+		x, edge_index, _, batch, _ = self.pool3(x, edge_index, edge_attr, batch)
 
 		mean_pool = global_mean_pool(x, batch)
 		max_pool = global_max_pool(x, batch)
