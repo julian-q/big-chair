fe8614ecc3191cee6ff6a688f1b2c7e25e71c982
julian-q <julianhquevedo@gmail.com>
2022-03-12 01:18:22 -0800

    eval good

--- evaluate_grad_cache.py
+++ evaluate_grad_cache.py
@@ -5,7 +5,7 @@ from dataset_pyg import AnnotatedMeshDataset
 from torch_geometric.loader import DataLoader
 from models import DescriptionEncoder, MeshEncoder
 import random
-
+from tqdm import tqdm
 import argparse
 
 argp = argparse.ArgumentParser()
@@ -34,32 +34,39 @@ def evaluate(eval_dataset, desc_encoder, mesh_encoder, device="cpu"):
 
     eval_dataloader = DataLoader(eval_dataset, batch_size=len(eval_dataset), shuffle=False)
 
-    total_val_acc = torch.tensor([0], dtype=torch.float).to(device)
-    for i_batch, batch in enumerate(eval_dataloader):
+    desc_embeddings = torch.empty(0, 128)
+    mesh_embeddings = torch.empty(0, 128)
+
+    for batch_i in tqdm(eval_dataloader):
+        print(torch.cuda.memory_summary())
+
+        batch.to(device)
         batch_descs, batch_meshes = batch.descs, batch
         # since each mesh may have differing numbers of descriptions, we sample a fixed
         # number (self.descs_per_mesh) of them for each mesh in order to standardize
         # memory usage
         sampled_descs = [random.choices(descs, k=args.descs_per_mesh) for descs in batch_descs]
-        tokenized_descs = desc_encoder.tokenize(sampled_descs) 
+        tokenized_descs = desc_encoder.tokenize(sampled_descs).to(device)
         
-        desc_embeddings = desc_encoder(tokenized_descs)
-        mesh_embeddings = mesh_encoder(batch_meshes)
+        desc_embeddings_i = desc_encoder(tokenized_descs).cpu()
+        mesh_embeddings_i = mesh_encoder(batch_meshes).cpu()
+        desc_embeddings = torch.cat([desc_embeddings, desc_embeddings_i], dim=0) 
+        mesh_embeddings = torch.cat([mesh_embeddings, mesh_embeddings_i], dim=0)
 
-        # logits_per_mesh = mesh_embeddings @ desc_embeddings
-        logits_per_desc = desc_embeddings @ mesh_embeddings
+    logits_per_desc = desc_embeddings @ mesh_embeddings.T
         
-        n_desc = desc_embeddings.shape[0]
-        n_mesh = mesh_embeddings.shape[0]
-        descs_per_mesh = n_desc // n_mesh
+    n_desc = desc_embeddings.shape[0]
+    n_mesh = mesh_embeddings.shape[0]
+    descs_per_mesh = n_desc // n_mesh
 
-        # target distributions
-        targets_per_desc = torch.zeros(n_desc, n_mesh).to(desc_embeddings.device)
-        # one-hot distribution for single matching mesh
-        targets_per_desc[torch.arange(n_desc), 
-                         torch.arange(n_mesh).repeat_interleave(descs_per_mesh)] = 1
+    # target distributions
+    targets_per_desc = torch.zeros(n_desc, n_mesh).to(desc_embeddings.device)
+    # one-hot distribution for single matching mesh
+    targets_per_desc[torch.arange(n_desc), 
+                        torch.arange(n_mesh).repeat_interleave(descs_per_mesh)] = 1
 
-        total_val_acc += top_5_eval(logits_per_desc, targets_per_desc)
+    total_val_acc = top_5_eval(logits_per_desc, targets_per_desc)
+
     return total_val_acc.item()
 
 device = 'cpu'
@@ -69,5 +76,5 @@ desc_encoder.load_state_dict(torch.load(args.name + "/" + args.name + "_desc_par
 mesh_encoder = MeshEncoder(128).to(device)
 mesh_encoder.load_state_dict(torch.load(args.name + "/" + args.name + "_mesh_parameters.pt"))
 val_dataset = torch.load("dataset/processed/val_set.pt")
-print("Val Accuracy: ", evaluate(val_dataset, desc_encoder, mesh_encoder))
+print("Val Accuracy: ", evaluate(val_dataset, desc_encoder, mesh_encoder, device=device))
 
