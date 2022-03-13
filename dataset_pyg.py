d58246595b99cf1e27a4450e968e9eee7755a9fd
sidsquid <sidney.hough@gmail.com>
2022-03-13 11:51:40 -0700

    f

--- dataset_pyg.py
+++ dataset_pyg.py
@@ -60,68 +60,68 @@ class AnnotatedMeshDataset(InMemoryDataset):
         # processing graphs
         graphs = []
         print_descs = True
-        for graph_name in os.listdir(os.path.join(self.processed_dir, 'graphs')):
-            g = torch.load(os.path.join(self.processed_dir, 'graphs', graph_name))
-            if isinstance(g.descs[0], str):
-                g.descs = [s.split('@')[0] for s in g.descs]
-            if print_descs:
-                print(g.descs)
-                print_descs = False
-            torch.save(g, os.path.join(self.processed_dir, 'graphs', graph_name))
-            graphs.append(g)
-        # for obj_class in ['Table']: # os.listdir(os.path.join('dataset', 'colored_models')):
-        #     for obj_folder in tqdm(os.listdir(os.path.join('dataset', 'colored_models', obj_class))):
-        #         # if os.path.exists(os.path.join(self.processed_dir, 'graphs', obj_folder + '.pt')):
-        #         #     g = torch.load(os.path.join(self.processed_dir, 'graphs', obj_folder + '.pt'))
-        #         #     graphs.append(g)
-        #         #     continue
-        #         print('loading', os.path.join('dataset', 'colored_models', obj_class, obj_folder, 'model.obj'))
-        #         print(os.path.getsize(os.path.join('dataset', 'colored_models', obj_class, obj_folder, 'model.obj')))
-        #         mesh = trimesh.load(os.path.join('dataset', 'colored_models', obj_class, obj_folder, 'model.obj'), force='mesh')
-        #         print('trimesh load done')
+        # for graph_name in os.listdir(os.path.join(self.processed_dir, 'graphs')):
+        #     g = torch.load(os.path.join(self.processed_dir, 'graphs', graph_name))
+        #     if isinstance(g.descs[0], str):
+        #         g.descs = [s.split('@')[0] for s in g.descs]
+        #     if print_descs:
+        #         print(g.descs)
+        #         print_descs = False
+        #     torch.save(g, os.path.join(self.processed_dir, 'graphs', graph_name))
+        #     graphs.append(g)
+        for obj_class in ['Table']: # os.listdir(os.path.join('dataset', 'colored_models')):
+            for obj_folder in tqdm(os.listdir(os.path.join('dataset', 'colored_models', obj_class))):
+                # if os.path.exists(os.path.join(self.processed_dir, 'graphs', obj_folder + '.pt')):
+                #     g = torch.load(os.path.join(self.processed_dir, 'graphs', obj_folder + '.pt'))
+                #     graphs.append(g)
+                #     continue
+                print('loading', os.path.join('dataset', 'colored_models', obj_class, obj_folder, 'model.obj'))
+                print(os.path.getsize(os.path.join('dataset', 'colored_models', obj_class, obj_folder, 'model.obj')))
+                mesh = trimesh.load(os.path.join('dataset', 'colored_models', obj_class, obj_folder, 'model.obj'), force='mesh')
+                print('trimesh load done')
                 
-        #         # model_id = obj.split('.')[0]
+                # model_id = obj.split('.')[0]
                 
-        #         full_descs = self.model2desc[obj_folder]
-        #         print('n descs', len(full_descs))
-        #         adj_nouns = [self.get_adj_noun(self.parser(desc)) for desc in full_descs]
-        #         descs = [{'full_desc': desc, 'adj_noun': adj_noun} for desc, adj_noun in zip(full_descs, adj_nouns)] #  + '@' + adj_noun
-        #         print('descs done')
-        #         # convert trimesh into graph, where the vertex positions are
-        #         # the node features! we also attach an attribute that will
-        #         # store the natural language descriptions
-        #         unique_edges = mesh.edges_unique
-        #         reversed_unique_edges = np.fliplr(unique_edges)
-        #         edges = np.concatenate([unique_edges, reversed_unique_edges])
+                full_descs = self.model2desc[obj_folder]
+                print('n descs', len(full_descs))
+                adj_nouns = [self.get_adj_noun(self.parser(desc)) for desc in full_descs]
+                descs = [{'full_desc': desc, 'adj_noun': adj_noun} for desc, adj_noun in zip(full_descs, adj_nouns)] #  + '@' + adj_noun
+                print('descs done')
+                # convert trimesh into graph, where the vertex positions are
+                # the node features! we also attach an attribute that will
+                # store the natural language descriptions
+                unique_edges = mesh.edges_unique
+                reversed_unique_edges = np.fliplr(unique_edges)
+                edges = np.concatenate([unique_edges, reversed_unique_edges])
 
-        #         edge_lengths = mesh.edges_unique_length
-        #         edge_lengths = np.concatenate([edge_lengths, edge_lengths])
+                edge_lengths = mesh.edges_unique_length
+                edge_lengths = np.concatenate([edge_lengths, edge_lengths])
 
-        #         # # get rid of alpha channel
-        #         # vertex_colors = np.delete(mesh.visual.vertex_colors, 3, 1)
-        #         # if not os.path.exists(os.path.join('dataset', 'colored_models', obj_class, obj_folder, 'vertex_colors.pickle')):
-        #         #     continue
-        #         vertex_colors_file = open(os.path.join('dataset', 'colored_models', obj_class, obj_folder, 'vertex_colors.pickle'), 'rb')
-        #         pos2col = pickle.load(vertex_colors_file)
-        #         rounded_pos2col = {}
-        #         for key, value in pos2col.items():
-        #             rounded_key = tuple([round(c, 4) for c in key])
-        #             rounded_pos2col[rounded_key] = value
-        #         vertex_colors = []
-        #         for v in mesh.vertices:
-        #             vertex_colors.append([round(c, 4) for c in v])
-        #         vertex_colors = np.array(vertex_colors)
-        #         vertex_colors_file.close()
-        #         node_features = np.concatenate([mesh.vertices, vertex_colors], axis=1)
-        #         print('features done')
+                # # get rid of alpha channel
+                # vertex_colors = np.delete(mesh.visual.vertex_colors, 3, 1)
+                # if not os.path.exists(os.path.join('dataset', 'colored_models', obj_class, obj_folder, 'vertex_colors.pickle')):
+                #     continue
+                vertex_colors_file = open(os.path.join('dataset', 'colored_models', obj_class, obj_folder, 'vertex_colors.pickle'), 'rb')
+                pos2col = pickle.load(vertex_colors_file)
+                rounded_pos2col = {}
+                for key, value in pos2col.items():
+                    rounded_key = tuple([round(c, 4) for c in key])
+                    rounded_pos2col[rounded_key] = value
+                vertex_colors = []
+                for v in mesh.vertices:
+                    vertex_colors.append([round(c, 4) for c in v])
+                vertex_colors = np.array(vertex_colors)
+                vertex_colors_file.close()
+                node_features = np.concatenate([mesh.vertices, vertex_colors], axis=1)
+                print('features done')
 
-        #         g = Data(x= torch.tensor(node_features).to(torch.float), # torch.rand(mesh.vertices.shape[0], 30),
-        #                     edge_index=torch.tensor(edges.T),
-        #                     edge_attr=torch.tensor(edge_lengths).to(torch.float),
-        #                     model_id=obj_folder,
-        #                     descs=descs)
-        #         graphs.append(g)
-        #         torch.save(g, os.path.join(self.processed_dir, 'good_graphs', obj_folder + '.pt'))
+                g = Data(x= torch.tensor(node_features).to(torch.float), # torch.rand(mesh.vertices.shape[0], 30),
+                            edge_index=torch.tensor(edges.T),
+                            edge_attr=torch.tensor(edge_lengths).to(torch.float),
+                            model_id=obj_folder,
+                            descs=descs)
+                graphs.append(g)
+                torch.save(g, os.path.join(self.processed_dir, 'good_graphs', obj_folder + '.pt'))
         self.data, self.slices = self.collate(graphs)
         torch.save((self.data, self.slices), self.processed_paths[0])
 
