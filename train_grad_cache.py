b5befb37e09c2e8cf708c904a3b2ca1b28f8998a
sidsquid <sidney.hough@gmail.com>
2022-03-14 00:13:06 -0700

    f

--- train_grad_cache.py
+++ train_grad_cache.py
@@ -99,6 +99,9 @@ for epoch in range(args.epoch):
 
 			loss = gc(sampled_descs, batch_meshes) # GradCache takes care of backprop
 			optimizer.step()
+
+			print(loss.grad)
+
 			loss.detach().cpu()
 
 			print("batch " + str(i_batch) + ": " + str(loss.item()))
